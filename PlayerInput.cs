13ccdeaa0769d71715cdd5e33d310ba08fd07a55
foreverplane <foreverplane@gmail.com>
2022-10-25 19:00:55 +0300

    fix

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [DefaultExecutionOrder(-999)]
-public class PlayerInputSingleton : Singleton<PlayerInputSingleton> {
+public class PlayerInput : Singleton<PlayerInput> {
 
 	public bool JumpIsPressed => _PlayerControls.ActionMap.Jump.IsPressed();
-	public Vector2 MoveVector => _PlayerControls.ActionMap.RightMouseButton.IsPressed() ? 
-		_PlayerControls.ActionMap.MoveWithMouse.ReadValue<Vector2>() : 
+	public Vector2 MoveVector => _PlayerControls.ActionMap.RightMouseButton.IsPressed() ?
+		_PlayerControls.ActionMap.MoveWithMouse.ReadValue<Vector2>() :
 		_PlayerControls.ActionMap.Move.ReadValue<Vector2>();
 
+	public bool IsRightMousePressed => _PlayerControls.ActionMap.RightMouseButton.IsPressed();
+	public bool IsLeftMousePressed => _PlayerControls.ActionMap.LeftMouseButton.IsPressed();
 	private PlayerControls _PlayerControls;
 	void Awake() {
 		_PlayerControls = new PlayerControls();
